6978d47c8778aed3da463617242f3ccc04927dba
Murilo Santos <muriloscunha23@gmail.com>
2024-05-31 17:16:03 -0300

    ajuste merge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,12 +17,8 @@ import { LoginUserComponent } from './client-area/login-user/login-user.componen
 import {CreditCardComponent} from "./client-area/credit-card/credit-card.component";
 
 import { ChangeClientComponent } from './client-area/change-client/change-client.component';
-<<<<<<< Updated upstream
-import {ListOrdersComponent} from "./backoffice/list-orders/list-orders.component";
-=======
 import { ShoppingCarComponent } from './client-area/shopping-car/shopping-car.component';
->>>>>>> Stashed changes
-
+import { ListOrdersComponent } from "./backoffice/list-orders/list-orders.component";
 
 
 const routes: Routes = [
@@ -78,16 +74,13 @@ const routes: Routes = [
     path: 'dados-pessoais',
     component: ChangeClientComponent
   },
-<<<<<<< Updated upstream
-
   {
     path: 'list-orders',
     component: ListOrdersComponent
-=======
+  },
   {
     path: 'carrinho',
     component: ShoppingCarComponent
->>>>>>> Stashed changes
   }
 ];
 
